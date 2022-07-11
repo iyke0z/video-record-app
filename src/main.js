29d90e24e126e5ea9fe9e19f250d57555c656056
ikebeelion <ianikwe@next-itservices.com>
2022-07-11 23:24:09 +0100

    update

--- src/main.js
+++ src/main.js
@@ -4,6 +4,7 @@ import router from './routes'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
+// import 'font-awesome/css/font-awesom'
 
 createApp(App)
     .use(router)
