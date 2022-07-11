4e0a4a264340f070e791f4510b6b52104946c84b
ikebeelion <ianikwe@next-itservices.com>
2022-07-11 23:10:40 +0100

    Update main.js

--- src/main.js
+++ src/main.js
@@ -2,12 +2,10 @@ import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
 import 'jquery/dist/jquery.min.js'
-import $ from 'jquery'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
-window.$ = $
+
 createApp(App)
-    .use($)
     .use(router)
     .mount('#app')
