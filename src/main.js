dea4fb29e8e46f6e67876fe927ba87f9e97b9119
ikebeelion <ianikwe@next-itservices.com>
2022-07-11 23:15:48 +0100

    push update

--- src/main.js
+++ src/main.js
@@ -1,7 +1,6 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './routes'
-import 'jquery/dist/jquery.min.js'
 import 'bootstrap';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import '@fortawesome/fontawesome-free/css/all.min.css'
